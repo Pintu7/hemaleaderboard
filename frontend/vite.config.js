29b21bb5b8827811479ee26bb21fb34c4cb0d08b
Pintu7 <36792043+Pintu7@users.noreply.github.com>
2025-07-25 15:22:51 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,31 @@
-<<<<<<< HEAD
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true // se usi vite dev server
-  }
-=======
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Copia il file di configurazione Azure SWA
+  copyFileSync('./staticwebapp.config.json', 'dist/staticwebapp.config.json')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': 'http://localhost:5000',
+      },
     },
-  },
->>>>>>> parent of cb1e7bf (frontend)
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+      },
+    },
+    define: {
+      'process.env': {},
+      __VITE_API_URL__: JSON.stringify(env.VITE_API_URL || ''),
+    },
+  }
 })
