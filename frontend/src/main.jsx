0aab52da2073de7522ef717a4333addac2f573c3
Pintu7 <36792043+Pintu7@users.noreply.github.com>
2025-07-25 14:10:26 +0200

    minuscole

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,10 +18,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/classifica" element={<Classifica />} />
         <Route path="/homepage" element={<NuovaPagina />} />
-        <Route path="/Leaderboard" element={<Leaderboard />} />
-         <Route path="/Ruleset" element={<Ruleset />} />
-         <Route path="/Tournaments" element={<Tournaments />} />
-         <Route path="/TournamentLeaderboard" element={<TournamentLeaderboard />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+         <Route path="/ruleset" element={<Ruleset />} />
+         <Route path="/tournaments" element={<Tournaments />} />
+         <Route path="/tournamentLeaderboard" element={<TournamentLeaderboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
